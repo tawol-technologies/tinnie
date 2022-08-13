be68531dfc707a5ca7efd50baca846447455f165
Ayomide Oyediran <oyediranjoysbright@gmail.com>
2022-08-13 08:25:45 +0100

    update utilities

--- enums/ErrorMessage.ts
+++ enums/ErrorMessage.ts
@@ -4,5 +4,7 @@ export enum ErrorMessage {
     INCOMPLETE_POST_CODE = "Postal code should be 6 digit length",
     WRONG_PIN = 'The length shoud be ',
     WRONG_PHONE_NUMBER = 'Must be a valid phone number',
-    GENDER_LENGTH = 'Gender can only be 4 or 6 character long'
+    GENDER_LENGTH = 'Gender can only be 4 or 6 character long',
+    MUST_BE_PERCENTAGE = 'Percentage must be number between 0 and 100',
+    MUST_BE_COUPON_CODE = 'Coupon code must be alphanumeric characters only'
 }
