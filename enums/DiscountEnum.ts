e1ff9a620bc63749e1fb361cf10342d87b114664
Ayomide Oyediran <oyediranjoysbright@gmail.com>
2022-08-13 09:03:36 +0100

    add list of values service

---
+++ enums/DiscountEnum.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+export enum DiscountTargetType {
+    ALL = 'All',
+    SPECIFIC_PRODUCT = 'Specific Product',
+    AMOUNT_PURCHASED = 'Amount Purchased'
+}
+export enum DiscountType {
+    PERCENTAGE = 'percentage',
+    FIGURE = 'figure'
+}
