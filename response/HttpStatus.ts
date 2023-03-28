e4cb7324f07a6d81a8290cc82375706537da132c
Ayomide Joysbright Oyediran <81700789+Oyejoysbright@users.noreply.github.com>
2023-03-28 07:13:12 +0100

    modify httpstatus

--- response/HttpStatus.ts
+++ response/HttpStatus.ts
@@ -1,10 +1,17 @@
 /* eslint-disable no-unused-vars */
 export enum HttpStatus {
+    // SUCCESSFUL
     OK = 200,
     CREATED = 201,
+
+    // CLIENT ERROR
     BAD_REQUEST = 400,
-    CONFLICT = 409,
+    UNAUTHORIZED = 401,
     FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    CONFLICT = 409,
+    UNPROCESSIBLE_ENTITY = 422,
+
+    // SERVER ERROR
     INTERNAL_SERVER_ERROR = 500,
-    UNAUTHORIZED = 401
 }
