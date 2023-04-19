98dcb0a7ca12c7776f9f5dbd897d7e83e865ad04
Ayomide Joysbright Oyediran <81700789+Oyejoysbright@users.noreply.github.com>
2023-04-19 16:23:59 +0100

    update rest controller

--- others/RestConnector.ts
+++ others/RestConnector.ts
@@ -6,7 +6,7 @@ export default class RestConnector {
   static exchange<T>(config: IExchangeConfig<T>, handlers: IExchangeHandler<T>) {
     LogService.info(`Calling URL :: ${config.url}\n PAYLOAD:: `);
     LogService.info(config.data);
-    axios(config).then((res) => {
+    axios(config).then((res:any) => {
       handlers.onSuccess && handlers.onSuccess(res.data, res.status, {...res});
     }).catch((reason) => {
       handlers.onFailure && handlers.onFailure(reason);
