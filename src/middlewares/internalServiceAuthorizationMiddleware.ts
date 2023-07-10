498ff2eb3ed2c48d545f2142b6ecf81f59d6646e
Ayomide Joysbright Oyediran <81700789+Oyejoysbright@users.noreply.github.com>
2023-07-10 12:22:43 +0100

    add whitelisting to client access check

--- src/middlewares/internalServiceAuthorizationMiddleware.ts
+++ src/middlewares/internalServiceAuthorizationMiddleware.ts
@@ -2,13 +2,18 @@ import { NextFunction, Request, Response } from "express";
 import RestConnector from "../others/RestConnector";
 import { ResponseMessage } from "../response/ResponseMessage";
 import { ResponseBuilder } from "../response/ResponseBody";
+import string from "../others/string";
 
-export const checkClientServiceAccess = async (req: Request, _res: Response, next: NextFunction) => {
+export const checkClientServiceAccess = async (req: Request, _res: Response, next: NextFunction, whitelistPaths?: string[]) => {
+  // Give access to unblocked APIs
+  if (whitelistPaths && string.isUrlMatch(whitelistPaths, req.path)) {
+    return next();
+  }
   
-    if (!req.headers.authorization) {
-      next(ResponseBuilder.getInstance().badRequest(ResponseMessage.MISSING_CREDENTIALS));
-      return;
-    }
+  if (!req.headers.authorization) {
+    next(ResponseBuilder.getInstance().badRequest(ResponseMessage.MISSING_CREDENTIALS));
+    return;
+  }
   const token = req.headers.authorization.substring(6);
   const service = process.env.SERVICE_NAME;
   await RestConnector.exchangeAsync({
